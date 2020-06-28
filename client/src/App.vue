b4e4c6990ce558f5702e5852955c5200f1bf08de
LucianMurmurache <48526455+lucianmurmurache@users.noreply.github.com>
2020-06-28 20:26:26 +0200

    Added client folder after heroku push
    In order to avoid uploading the client folder to heroku,
    I temporarily added it to gitignore.

---
+++ client/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <a href="https://murmurache.com" target="_blank" rel="noopener">
+      <img alt="Mini posts logo" src="./assets/logo.png" />
+    </a>
+    <PostComponent />
+  </div>
+</template>
+
+<script>
+import PostComponent from "./components/PostComponent.vue";
+
+export default {
+  name: "App",
+  components: {
+    PostComponent
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ client/src/components/PostComponent.vue
@@ -0,0 +1,194 @@
+<template>
+  <div class="container">
+    <h1>Latest Posts</h1>
+    <div class="create-post">
+      <label for="create-post">Add a post</label>
+      <input
+        type="text"
+        id="create-post"
+        v-model="text"
+        v-on:keyup.enter="createPost"
+        placeholder="Write something..."
+      />
+      <button v-on:click="createPost">Post</button>
+    </div>
+    <hr />
+    <p class="error" v-if="error">{{error}}</p>
+    <div class="posts-container">
+      <div
+        class="post"
+        v-for="(post, index) in posts"
+        v-bind:item="post"
+        v-bind:index="index"
+        v-bind:key="post._id"
+      >
+        <span aria-hidden="true" v-on:click="deletePost(post._id)">x</span>
+        {{`${post.createdAt.getDate()}/${post.createdAt.getMonth()}/${post.createdAt.getFullYear()} `}}
+        <p class="text">{{post.text}}</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import PostService from "../PostService";
+
+export default {
+  name: "PostComponent",
+  data() {
+    return {
+      posts: [],
+      error: "",
+      text: ""
+    };
+  },
+  async created() {
+    try {
+      this.posts = await PostService.getPosts();
+    } catch (err) {
+      this.error = err.message;
+    }
+  },
+  methods: {
+    async createPost() {
+      if (!this.text) {
+        // do not submit if field is empty
+        this.hasError = true;
+        return;
+      }
+      await PostService.insertPost(this.text);
+      this.posts = await PostService.getPosts();
+      this.text = "";
+    },
+    async deletePost(id) {
+      await PostService.deletePost(id);
+      this.posts = await PostService.getPosts();
+    }
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+/*
+* Color palette:
+ * #80f07d Light Green
+ * #73e067 Malachite
+ * #317b22 Ao English
+ * #2a4d14 Lincoln Green
+*/
+@import url(https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;700&display=swap);
+
+* {
+  padding: 0;
+  box-sizing: border-box;
+}
+
+body {
+  font-family: "Roboto", sans-serif;
+}
+
+h1 {
+  color: #000;
+}
+div.container {
+  max-width: 50rem;
+  margin: 0 auto;
+}
+
+p.error {
+  color: #e1efe6;
+  border: 0.063rem solid #2a4d14;
+  background-color: #ff0000;
+  padding: 0.7rem;
+  margin-bottom: 1rem;
+}
+
+div.post {
+  position: relative;
+  color: #000;
+  border: 0.063rem solid #2a4d14;
+  border-radius: 0.4rem;
+  background-color: #73e067;
+  padding: 0.6rem 0.6rem 2rem 0.6rem;
+  box-shadow: 0rem 0rem 0.5rem rgba(0, 0, 0, 0.4);
+  margin-bottom: 1rem;
+  overflow: auto;
+}
+
+div.post p.text {
+  text-align: center;
+}
+
+/*===== Input =====*/
+div.create-post {
+  display: inline;
+}
+
+div.create-post label {
+  font-size: 1.2rem;
+  margin-bottom: 0.3rem;
+  color: #000;
+  display: block;
+  transition: all 0.3s;
+}
+
+div.create-post input {
+  color: #000;
+  font-size: 1.2rem;
+  margin: 0 auto;
+  padding: 0.5rem 1rem;
+  border: 0.02rem solid #2a4d14;
+  border-radius: 0.4rem;
+  background-color: #fff;
+  box-shadow: 0rem 0rem 0.5rem rgba(0, 0, 0, 0.4);
+  width: 70%;
+}
+
+div.create-post button {
+  background-color: #fff;
+  border: 0.02rem solid #2a4d14;
+  border-radius: 0.4rem;
+  color: #000;
+  padding: 0.63rem 1.2rem;
+  text-align: center;
+  text-decoration: none;
+  font-size: 1rem;
+  margin: 0.1rem 0.6rem;
+  box-shadow: 0rem 0rem 0.5rem rgba(0, 0, 0, 0.4);
+  cursor: pointer;
+}
+
+div.create-post button:hover {
+  background-color: #80f07d;
+}
+
+/*===== Delete post btn =====*/
+div.post span {
+  text-align: right;
+  border: none;
+  font-size: 1.25rem;
+  padding: 0;
+  cursor: pointer;
+  font-weight: bold;
+  color: #000;
+  background: transparent;
+  margin-left: 95%;
+}
+
+div.created-at {
+  position: absolute;
+  top: 0;
+  left: 0;
+  padding: 0.4rem 1rem 0.4rem 1rem;
+  color: #000;
+  background-color: #317b22;
+  font-size: 0.9rem;
+}
+
+p.text {
+  font-size: 1.3rem;
+  font-weight: 700;
+  margin-bottom: 0;
+}
+</style>
